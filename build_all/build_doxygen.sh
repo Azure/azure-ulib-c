cbd17feed61ca19f6763592702c28416cff5b54e
Dane Walton <dawalton@microsoft.com>
2019-06-28 13:03:56 -0700

    working doxy script

---
+++ build_all/build_doxygen.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+script_dir=$(cd "$(dirname "$0")" && pwd)
+echo $script_dir
+repo_dir=$script_dir/..
+echo $repo_dir
+
+if [ ! -d doxy/ ]; then
+    mkdir doxy
+fi
+
+pushd doxy
+for filename in $repo_dir/inc/*.h
+do
+    if [ ! -d inc/ ]; then
+        mkdir inc
+    fi
+    echo $filename
+    file=${filename##*/}
+    perl -0pe 's/MU_DEFINE_ENUM\(\s*?(\w*),([\s\w\W]*)\)/typedef enum \1_TAG {\2} \1;/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?)(\);)/\1 \2();/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6, \7 \8);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6, \7 \8, \9 \10);/g' $filename > inc/$file
+done
+
+for filename in $repo_dir/config/*.h
+do
+    if [ ! -d config/ ]; then
+        mkdir config
+    fi
+    echo $filename
+    file=${filename##*/}
+    perl -0pe 's/MOCKABLE_FUNCTION\(, (.*?), (.*?)(\);)/\1 \2();/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6, \7 \8);/g;
+                s/MOCKABLE_FUNCTION\(, (.*?), (.*?),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*),\s*([\w\*_\s]*), (\w*)\);/\1 \2(\3 \4, \5 \6, \7 \8, \9 \10);/g' $filename > config/$file
+done
+
+popd
+
+pushd $repo_dir/docs
+doxygen $repo_dir/docs/Doxyfile
+popd
+
+rm -r $repo_dir/doxy
