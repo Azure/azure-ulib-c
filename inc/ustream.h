75a44ea9a013c79ec191ec6c26287a5e4fa083b1
Dane Walton <dawalton@microsoft.com>
2019-07-17 14:34:35 -0700

    revert api

--- inc/ustream.h
+++ inc/ustream.h
@@ -31,49 +31,33 @@ extern "C" {
  * 
  * @return  void
  */
-typedef void (*USTREAM_BUFFER_RELEASE_CALLBACK)(void*);
-
-/**
- * @brief   Signature of the function to release inner buffer control block
- * 
- * @param[in]   void*       void pointer to memory that needs to be free'd
- * 
- * @return  void
- */
-typedef void (*USTREAM_INNER_BUFFER_RELEASE_CALLBACK)(void*);
+typedef void (*AZIOT_USTREAM_BUFFER_RELEASE_CALLBACK)(void*);
 
 /**
  * @brief   Factory to create a new uStream.
  *
  *  This factory creates a uStream that handles the content of the provided buffer. As a result,
  *      it will return a {@link USTREAM}* with this content. The created uStream takes ownership of the
- *      passed memory and will release the memory with the passed #USTREAM_BUFFER_RELEASE_CALLBACK function when
+ *      passed memory and will release the memory with the passed #AZIOT_USTREAM_BUFFER_RELEASE_CALLBACK function when
  *      the ref count goes to zero.
  *
- * @param[in,out]   ustream_return                     The {@link USTREAM}* to initialize.
- * @param[in]       buffer                      The <tt>const uint8_t* const</tt> that points to a memory position where the buffer starts.
- *                                              It cannot be <tt>NULL</tt>.
- * @param[in]       buffer_length               The <tt>size_t</tt> with the number of <tt>uint8_t</tt> in the provided buffer.
- * @param[in]       release_buffer              The #USTREAM_BUFFER_RELEASE_CALLBACK function that will be called for the inner 
- *                                              buffer once all the references to the uStream are released. If <tt>NULL</tt> is 
- *                                              passed, the data is assumed to be constant with no need to be free'd. In other words, 
- *                                              there is no need for notification that the memory can be released.
- *                                              As a default, users may use the standard <tt>free</tt> to release malloc'd memory.
- * @param[in]       release_inner_control_block The #USTREAM_INNER_BUFFER_RELEASE_CALLBACK function that will be called for the inner buffer once 
- *                                              all the references to the uStream are released. If <tt>NULL</tt> is passed, the data is assumed 
- *                                              to be constant with no need to be free'd. In other words, there is no need for notification 
- *                                              that the memory can be released. As a default, users may use the standard <tt>free</tt> to 
- *                                              release malloc'd memory.
+ * @param[in]   buffer          The <tt>const uint8_t* const</tt> that points to a memory position where the buffer starts.
+ *                              It cannot be <tt>NULL</tt>.
+ * @param[in]   buffer_length   The <tt>size_t</tt> with the number of <tt>uint8_t</tt> in the provided buffer.
+ * @param[in]   release_buffer  The #AZIOT_USTREAM_BUFFER_RELEASE_CALLBACK function that will be called for the inner 
+ *                              buffer once all the references to the uStream are released. If <tt>NULL</tt> is 
+ *                              passed, the data is assumed to be constant with no need to be free'd. In other words, 
+ *                              there is no need for notification that the memory can be released.
+ *                              As a default, users may use the standard <tt>free</tt> to release malloc'd memory.
  *
- * @return The {@link AZIOT_ULIB_RESULT} with the uStream interface.
- *          @retval     ULIB_SUCCESS                If the uStreamMulti appended the provided uStream with success.
+ * @return The {@link USTREAM}* with the uStream interface.
+ *          @retval not-NULL    If the uStream was created with success.
+ *          @retval NULL        If there is no memory to create the new uStream.
  */
-MOCKABLE_FUNCTION(, AZIOT_ULIB_RESULT, ustream_create,
-        USTREAM*, ustream_return,
+MOCKABLE_FUNCTION(, USTREAM*, ustream_create,
         const uint8_t* const, buffer,
         size_t, buffer_length,
-        USTREAM_BUFFER_RELEASE_CALLBACK, release_buffer,
-        USTREAM_INNER_BUFFER_RELEASE_CALLBACK, release_inner_control_block);
+        AZIOT_USTREAM_BUFFER_RELEASE_CALLBACK, release_buffer);
 
 
 /**
