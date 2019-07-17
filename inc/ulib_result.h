9f4fc17388401e7da59ca96e6ee07a6dae15aa2e
Dane Walton <dawalton@microsoft.com>
2019-07-17 14:14:30 -0700

    update error flag

--- inc/ulib_result.h
+++ inc/ulib_result.h
@@ -5,8 +5,8 @@
  * @file ulib_result.h
  */
 
-#ifndef AZURE_ULIB_C_INC_AZIOT_ULIB_RESULT_H_
-#define AZURE_ULIB_C_INC_AZIOT_ULIB_RESULT_H_
+#ifndef AZIOT_ULIB_RESULT_H_
+#define AZIOT_ULIB_RESULT_H_
 
 #include "azure_macro_utils/macro_utils.h"
 #include "umock_c/umock_c_prod.h"
@@ -18,7 +18,7 @@ extern "C" {
 /**
  * @brief   Error bit for error values.
  */
-#define ULIB_ERROR_FLAG 0X80
+#define AZIOT_ULIB_ERROR_FLAG 0X80
 
 /**
  * @brief   Enumeration of uLib results.
@@ -31,13 +31,13 @@ MU_DEFINE_ENUM(
     ULIB_EOF                        = 1,                        /**<End of fle (no more data to read) */
 
     //ERROR RESULTS
-    ULIB_OUT_OF_MEMORY_ERROR            = (ULIB_ERROR_FLAG | 0),    /**<Out of memory error */
-    ULIB_NO_SUCH_ELEMENT_ERROR          = (ULIB_ERROR_FLAG | 1),    /**<No such element error */
-    ULIB_ILLEGAL_ARGUMENT_ERROR         = (ULIB_ERROR_FLAG | 2),    /**<Illegal argument error */
-    ULIB_SECURITY_ERROR                 = (ULIB_ERROR_FLAG | 3),    /**<Security error */
-    ULIB_SYSTEM_ERROR                   = (ULIB_ERROR_FLAG | 4),    /**<System error */
-    ULIB_CANCELLED_ERROR                = (ULIB_ERROR_FLAG | 5),    /**<Cancelled error */
-    ULIB_BUSY_ERROR                     = (ULIB_ERROR_FLAG | 6)     /**<Busy error */
+    ULIB_OUT_OF_MEMORY_ERROR        = (AZIOT_ULIB_ERROR_FLAG | 0),    /**<Out of memory error */
+    ULIB_NO_SUCH_ELEMENT_ERROR      = (AZIOT_ULIB_ERROR_FLAG | 1),    /**<No such element error */
+    ULIB_ILLEGAL_ARGUMENT_ERROR     = (AZIOT_ULIB_ERROR_FLAG | 2),    /**<Illegal argument error */
+    ULIB_SECURITY_ERROR             = (AZIOT_ULIB_ERROR_FLAG | 3),    /**<Security error */
+    ULIB_SYSTEM_ERROR               = (AZIOT_ULIB_ERROR_FLAG | 4),    /**<System error */
+    ULIB_CANCELLED_ERROR            = (AZIOT_ULIB_ERROR_FLAG | 5),    /**<Cancelled error */
+    ULIB_BUSY_ERROR                 = (AZIOT_ULIB_ERROR_FLAG | 6)     /**<Busy error */
 );
 
 
@@ -45,4 +45,4 @@ MU_DEFINE_ENUM(
 }
 #endif  //__cplusplus
 
-#endif  //AZURE_ULIB_C_INC_AZIOT_ULIB_RESULT_H_
+#endif  //AZIOT_ULIB_RESULT_H_
