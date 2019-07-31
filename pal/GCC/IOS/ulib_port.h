10e36ca60520d8b05fc44ade5e98697dc449faf9
Dane Walton <dawalton@microsoft.com>
2019-07-31 09:25:54 -0700

    update port macros

--- pal/GCC/IOS/ulib_port.h
+++ pal/GCC/IOS/ulib_port.h
@@ -8,31 +8,31 @@
 
 
 // This iOS-specific header offers 3 strategies:
-//   AZURE_ULIB_C_ATOMIC_DONTCARE     -- no atomicity guarantee
-//   AZURE_ULIB_C_USE_STD_ATOMIC      -- C11 atomicity
-//   AZURE_ULIB_C_USE_GNU_C_ATOMIC    -- GNU-specific atomicity
+//   AZULIB_C_ATOMIC_DONTCARE     -- no atomicity guarantee
+//   AZULIB_C_USE_STD_ATOMIC      -- C11 atomicity
+//   AZULIB_C_USE_GNU_C_ATOMIC    -- GNU-specific atomicity
 
 #if defined(__GNUC__)
-#define AZURE_ULIB_C_USE_GNU_C_ATOMIC 1
+#define AZULIB_C_USE_GNU_C_ATOMIC 1
 #endif
 
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112)
-#define AZURE_ULIB_C_USE_STD_ATOMIC 1
-#undef AZURE_ULIB_C_USE_GNU_C_ATOMIC
+#define AZULIB_C_USE_STD_ATOMIC 1
+#undef AZULIB_C_USE_GNU_C_ATOMIC
 #endif
 
-#if defined(AZURE_ULIB_C_ATOMIC_DONTCARE)
+#if defined(AZULIB_C_ATOMIC_DONTCARE)
 #define COUNT_TYPE uint32_t
-#elif defined(AZURE_ULIB_C_USE_STD_ATOMIC)
+#elif defined(AZULIB_C_USE_STD_ATOMIC)
 #define COUNT_TYPE _Atomic uint32_t
-#else  // AZURE_ULIB_C_USE_GNU_C_ATOMIC
+#else  // AZULIB_C_USE_GNU_C_ATOMIC
 #define COUNT_TYPE uint32_t
-#endif // defined(AZURE_ULIB_C_ATOMIC_DONTCARE)
+#endif // defined(AZULIB_C_ATOMIC_DONTCARE)
 
 
 /*the following macros increment/decrement a ref count in an atomic way, depending on the platform*/
 /*The following mechanisms are considered in this order
-AZURE_ULIB_C_ATOMIC_DONTCARE does not use atomic operations
+AZULIB_C_ATOMIC_DONTCARE does not use atomic operations
 - will result in ++/-- used for increment/decrement.
 C11
 - will result in #include <stdatomic.h>
@@ -45,20 +45,20 @@ gcc
 */
 
 
-#if defined(AZURE_ULIB_C_ATOMIC_DONTCARE)
+#if defined(AZULIB_C_ATOMIC_DONTCARE)
 #define AZULIB_PORT_ATOMIC_INC_W(count) ++(*count)
 #define AZULIB_PORT_ATOMIC_DEC_W(count) --(*count)
 
-#elif defined(AZURE_ULIB_C_USE_STD_ATOMIC)
+#elif defined(AZULIB_C_USE_STD_ATOMIC)
 #include <stdatomic.h>
 #define AZULIB_PORT_ATOMIC_INC_W(count) atomic_fetch_add((count), 1)
 #define AZULIB_PORT_ATOMIC_DEC_W(count) atomic_fetch_sub((count), 1)
 
-#elif defined(AZURE_ULIB_C_USE_GNU_C_ATOMIC)
+#elif defined(AZULIB_C_USE_GNU_C_ATOMIC)
 #define AZULIB_PORT_ATOMIC_INC_W(count) __sync_add_and_fetch((count), 1)
 #define AZULIB_PORT_ATOMIC_DEC_W(count) __sync_sub_and_fetch((count), 1)
 
-#endif /*defined(AZURE_ULIB_C_USE_GNU_C_ATOMIC)*/
+#endif /*defined(AZULIB_C_USE_GNU_C_ATOMIC)*/
 
 #define AZULIB_PORT_THROW_HARD_FAULT      (*(char*)NULL = 0)
 
