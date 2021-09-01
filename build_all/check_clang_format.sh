d3ebb8e6519452ce65f8bdc3d8c83a80a41a48d5
Spencer McDonough <shmcdono@usc.edu>
2021-09-01 16:00:24 -0700

    check format scripts

---
+++ build_all/check_clang_format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clang-format --version
+find . \( -iname '*.h' -o -iname '*.c' \) -exec clang-format -i {} \;
+git status --untracked-files=no --porcelain
+if [[ `git status --untracked-files=no --porcelain` ]]; then
+  echo Some files were not formatted correctly according to the .clang-format file.
+  echo Please run clang-format to fix the issue by using this bash command at the root of the repo:
+  echo "find . \( -iname '*.h' -o -iname '*.c' \) -exec clang-format -i {} \;"
+  exit 1
+fi
+echo Success, all files are formatted correctly according to the .clang-format file.
+exit 0
